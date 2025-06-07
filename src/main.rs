6223fd5f91acbf542873ebb6f398347e76024257
KisChang <734615869@qq.com>
2025-06-07 16:47:10 +0800

    support mutil devices

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
+use crossterm::execute;
 use crossterm::terminal::{Clear, ClearType};
-use crossterm::{execute, terminal::SetSize};
 use nvml_wrapper::error::NvmlError;
 use nvml_wrapper::Nvml;
 use std::collections::VecDeque;
-use std::io::{self, Write};
+use std::io::{self};
 use std::thread;
 use std::time::Duration;
 use sysinfo::System;
@@ -46,32 +46,22 @@ fn main() -> Result<(), NvmlError> {
     let mut sys = System::new_all();
     let nvml = Nvml::init()?;
 
-    /*let device_count = nvml.device_count()?;
-    println!("Found {} GPU(s)", device_count);
-    for i in 0..device_count {
-        let device = nvml.device_by_index(i)?;
-    }*/
+    // 初始化
+    let mut usage_map = vec![];
+    let device_count = nvml.device_count()?;
+    for _ in 0..device_count {
+        usage_map.push(FixedVec::new(100));
+    }
 
-    let mut usage_history = FixedVec::new(100);
-
-    // Init Terminal size
-    execute!(io::stdout(), SetSize(120, 100));
+    // TODO check Terminal size
+    // execute!(io::stdout(), SetSize(120, 100));
 
     loop {
         // refresh SYS usage info
         sys.refresh_all();
 
-        // get GPU usage info
-        let device = nvml.device_by_index(0)?;
-        let name = device.name()?;
-        let mem_info = device.memory_info()?;
-        let utilization = device.utilization_rates()?;
-        let temperature = device.temperature(nvml_wrapper::enum_wrappers::device::TemperatureSensor::Gpu)?;
-
-        usage_history.push((0f32, utilization.gpu as f32));
-
         // 清屏
-        // print!("\x1B[2J\x1B[1;1H");
+        print!("\x1B[2J\x1B[1;1H");
         execute!(io::stdout(), Clear(ClearType::All));
 
         // CPU Info
@@ -80,24 +70,39 @@ fn main() -> Result<(), NvmlError> {
         let total_memory = sys.total_memory(); // in kilobytes
         let used_memory = sys.used_memory();   // in kilobytes
         println!(
-            "CPU Usage: {:.2}% Memory: used {} MB / total {} MB",
+            "CPU Usage: {:.2}%; Memory: {} / {} MB",
             cpu_usage,
             used_memory / 1024 / 1024,
             total_memory / 1024 / 1024
         );
-        // GPU Info
-        println!("GPU {} Temperature: {}°C; Utilization: {}%; Memory: used {} MB / {} MB; "
-                 , name
-                 , temperature
-                 , utilization.gpu
-                 , mem_info.used / 1024 / 1024, mem_info.total / 1024 / 1024
-        );
-        Chart::new(100, 50, 0.0, 100.0)
-            .lineplot(&Shape::Lines(&(0..100).map(|i| usage_history.data[i]).collect::<Vec<_>>()))
-            .y_axis_style(LineStyle::Solid)
-            .x_axis_style(LineStyle::Solid)
-            .display();
 
+        // GPU Info
+        for ind in 0..device_count {
+            // 这里肯定可以获取到，前面正常初始化了
+            let usage_history = usage_map.get_mut(ind as usize).unwrap();
+
+            let device = nvml.device_by_index(ind)?;
+            // get GPU usage info
+            let name = device.name()?;
+            let mem_info = device.memory_info()?;
+            let utilization = device.utilization_rates()?;
+            let temperature = device.temperature(nvml_wrapper::enum_wrappers::device::TemperatureSensor::Gpu)?;
+
+            usage_history.push((0f32, utilization.gpu as f32));
+
+            println!("GPU {}.{} Temperature: {}°C; Utilization: {}%; Memory: {} / {} MB "
+                     , ind
+                     , name
+                     , temperature
+                     , utilization.gpu
+                     , mem_info.used / 1024 / 1024, mem_info.total / 1024 / 1024
+            );
+            Chart::new(100, 50, 0.0, 100.0)
+                .lineplot(&Shape::Lines(&(0..100).map(|i| usage_history.data[i]).collect::<Vec<_>>()))
+                .y_axis_style(LineStyle::Solid)
+                .x_axis_style(LineStyle::Solid)
+                .display();
+        }
         thread::sleep(Duration::from_millis(500));
     }
 }
